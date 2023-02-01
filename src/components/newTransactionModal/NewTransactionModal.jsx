c588102cf6646b058add68ec9e9b79fd3f74a7d8
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2023-02-01 17:36:24 -0300

    criado entradas e saidas checked no modal

--- src/components/newTransactionModal/NewTransactionModal.jsx
+++ src/components/newTransactionModal/NewTransactionModal.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from "phosphor-react";
+import * as RadioGroup from '@radix-ui/react-radio-group';
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
 
 export const Overlay = styled(Dialog.Overlay)`
   position: fixed;
@@ -19,11 +20,13 @@ export const Content = styled(Dialog.Content)`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  
   form {
     margin-top: 2rem;
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    
     input {
       border-radius: 6px;
       border: 0;
@@ -63,6 +66,42 @@ export const CloseButton = styled(Dialog.Close)`
   color: ${props => props.theme["gray-500"]};
 `;
 
+export const TransactionType = styled(RadioGroup.Root)`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+  margin-top: 0.5rem;
+`;
+
+export const TransactionTypeButton = styled(RadioGroup.Item)`
+  background: ${props => props.theme["gray-700"]};
+  padding: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  border-radius: 6px;
+  cursor: pointer;
+  border: 0;
+  color: ${props => props.theme["gray-300"]};
+  
+  svg {
+    color: ${props => props.variant === 'income' ? props.theme["green-300"] : props.theme["red-300"]};
+  }
+
+  &[data-state='unchecked']:hover {
+    transition: background-color 0.2s;
+    background: ${props => props.theme["gray-600"]};
+  }
+  &[data-state='checked'] {
+    color: ${props => props.theme.white};
+    background: ${props => props.variant === 'income' ? props.theme["green-500"] : props.theme["red-500"]};
+    svg {
+      color: ${props => props.theme.white};
+    }
+  }
+`;
+
 export function NewTransactionModal() {
     return (
       <Dialog.Portal>
@@ -79,7 +118,18 @@ export function NewTransactionModal() {
             <input type="text" placeholder="Descrição" required />
             <input type="number" placeholder="Preço" required />
             <input type="text" placeholder="Categoria" required />
-  
+
+            <TransactionType>
+                <TransactionTypeButton variant="income" value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                </TransactionTypeButton>
+                <TransactionTypeButton variant="outcome" value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                </TransactionTypeButton>
+            </TransactionType>
+
             <button type="submit">
               Cadastrar
             </button>

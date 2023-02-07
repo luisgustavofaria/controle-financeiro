c9e1ec36b32c3f8ecdfbf544b8267d097b145a23
Luis Gustavo Faria <luis.gustavo.fariac@gmail.com>
2023-02-07 19:33:33 -0300

    corrigido

--- src/lib/Axios.js
+++ src/lib/Axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://json-server-controle-financeiro.vercel.app/transactions',
+  baseURL: 'http://localhost:3333',
 })
